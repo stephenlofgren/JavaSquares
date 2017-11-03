3a3b5060e5f4034abd99b394fa7af0b08362cd1a
Stephen Lofgren <stephenlofgren@hotmail.com>
2017-11-03 07:03:36 -0600

    start.spring.io stub

---
+++ src/main/java/com/squares/JavaSquares/JavaSquaresApplication.java
@@ -0,0 +1,12 @@
+package com.squares.JavaSquares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSquaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSquaresApplication.class, args);
+	}
+}
